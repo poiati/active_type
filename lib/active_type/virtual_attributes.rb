5e415325cfc95c4164eae00375afa495403fdc92
Paulo Poiati <paulogpoiati@gmail.com>
2015-09-14 21:58:58 -0300

    Better handling of untyped attributes.
    Prevent an unnecessary connection to the database when no type is
    specified.

--- lib/active_type/virtual_attributes.rb
+++ lib/active_type/virtual_attributes.rb
@@ -45,7 +45,7 @@ module ActiveType
       private
 
       def add_virtual_column(name, type, options)
-        type_caster = TypeCaster.get(type, @owner.connection)
+        type_caster = TypeCaster.get(type, (!type.nil? and @owner.connection))
         column = VirtualColumn.new(name, type_caster, options.slice(:default))
         @owner.virtual_columns_hash = @owner.virtual_columns_hash.merge(name.to_s => column)
       end
